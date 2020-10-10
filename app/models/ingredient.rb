0ef6b473fd07fbe232ff8d874ce4ad286f25fd00
patrick-rush <patrick.a.rush@gmail.com>
2020-10-10 18:49:00 -0400

    Done.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,8 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
+
+  def ingredient_attributes=(ingredient)
+    self.ingredient = Ingredient.find_or_create_by(name: ingredient[:name])
+    self.ingredient.update(ingredient)
+  end
 end
